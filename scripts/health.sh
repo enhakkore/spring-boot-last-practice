29721dd65fb6b95826d512b1e2bc119a7508f00d
enhakkore <ahn.ephraim@gmail.com>
2020-01-12 19:22:44 +0900

    Fix typo in health.sh

--- scripts/health.sh
+++ scripts/health.sh
@@ -20,7 +20,7 @@ do
     if [ ${UP_COUNT} -ge 1 ]
     then
         echo "> Health check 성공"
-        swithch_proxy
+        switch_proxy
         break
     else
         echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
